607f793c62f064008c2902d1a62728ecdb879433
ameerwebdeveloper <ameer.mohammed.dev@gmail.com>
2025-02-16 03:24:18 +0100

    first commit

---
+++ Course3/homework_1_4.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+
+int main()
+{
+    /*
+     std::cout << "Ameer Mohamemd\n\n";
+    std::cout << "I am 30" << std::endl;
+    
+    */
+
+    /*
+    std::cout << "M1" << "M2" << "M3" << "\n";
+    std::cout << "M1 " << "M2 " << "M3";
+    */
+
+    /*
+    std::cout << "The result of " << "8 + 10 = " << " " << 8 + 10;
+   
+   */
+
+    std::cout << "*********************************************** \n";
+    std::cout << "Name :" << "Ameer MOhammed" << "\n";
+    std::cout << "Age  :" << 30 << "\n";
+    std::cout << "City :" << "Flensburg" << "\n";
+    std::cout << "Country:" << "Germany" << "\n";
+    std::cout << "***********************************************\n";
+
+
+    std::cout << std::endl;
+
+    std::cout << "***********\n";
+    std::cout << "***********\n";
+    std::cout << "***********\n";
+    std::cout << "***********\n";
+
+    std::cout << std::endl;
+
+    std::cout << "I Love Programming!" << "\n\n";
+    std::cout << "I Promise to be th best developer ever!" << "\n\n";
+    std::cout << "I know it will take some time tp practice, but I will achive my goal" << "\n\n";
+    std::cout << "Best Regards!" << "\n\n";
+    std::cout << "Ameer Mohammed" << "\n\n";
+
+    std::cout << "*     *\n";
+    std::cout << "*     *\n";
+    std::cout << "*******\n";
+    std::cout << "*     *\n";
+    std::cout << "*     *\n";
+
+   
+
+    return 0;
+}
+
